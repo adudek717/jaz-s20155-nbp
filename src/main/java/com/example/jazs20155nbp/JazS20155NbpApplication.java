4a3d2d8e4ade92e76b253c2d82bc7b32657f6878
adudek717 <a.dudek717@gmail.com>
2021-07-03 16:10:32 +0200

    initial commit

---
+++ src/main/java/com/example/jazs20155nbp/JazS20155NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.jazs20155nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS20155NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS20155NbpApplication.class, args);
+	}
+
+}
